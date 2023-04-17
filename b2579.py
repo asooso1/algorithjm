1e989cb87cbc2fc8eda1965a62c3fca9ef37e1ce
asooso1 <asooso1@naver.com>
2023-04-17 20:39:47 +0900

    b2579 solved

--- b2579.py
+++ b2579.py
@@ -7,12 +7,13 @@ floors.reverse()
 
 # dp = [(0, 0, 0) for _ in range(N)]
 dp = [0] * (N + 1)
-
+dp[0] = floors[0]
 
 # dp[1] = (floors[0] + floors[1], floors[0)
 
 for i in range(1, N - 1):
     dp[i] = dp[i - 1] + max(floors[i],floors[i + 1])
+    
     
     # if i % 3 == 0:
     #     dp[i] = (dp[i - 1][0]
